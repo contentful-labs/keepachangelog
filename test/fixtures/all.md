1a57f5b090a5416ebd0e7d369d755a8626428768
Thomas Scholtes <thomas-scholtes@gmx.de>
2015-03-11 19:49:12 +0100

    Add tests

---
+++ test/fixtures/all.md
@@ -0,0 +1,16 @@
+# Change Log
+The prelude
+
+## Upcoming
+### Added
+- one added
+
+### Changed
+- this has changed
+
+### Removed
+- gone :(
+
+## v1.0.0
+### Added
+- the initial release
---
+++ test/test.js
@@ -0,0 +1,19 @@
+import path from 'path'
+import {readFileSync} from 'fs'
+import {expect} from 'chai'
+import {parse} from '../src'
+
+function readFixture(name) {
+  var p = path.resolve('test', 'fixtures', name) + '.md'
+  return readFileSync(p, {encoding: 'utf8'})
+}
+
+describe('changelog', function() {
+
+  it('checks fixtures', function() {
+    var source = readFixture('all')
+    var changelog = parse(source)
+    expect(changelog.build()).to.equal(source)
+  });
+
+});
