fc9edaa54326ce18331627560852a4bd5c375f9f
Joshua Wood <josh@joshuawood.net>
2021-04-19 16:46:19 -0700

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,6 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
-gulp.task('default', ['compile']);
-
 gulp.task('compile', function () {
     return gulp.src('src/**/*.js')
         .pipe(babel({optional: ['runtime']}))
@@ -14,3 +12,5 @@ gulp.task('compile', function () {
 gulp.task('watch', function () {
   gulp.watch('src/**/*.js', ['compile']);
 });
+
+gulp.task('default', gulp.series('compile'));
